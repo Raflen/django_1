5f0289859c596f9766847e9830a2b04a8f3fcd00
Rafal Lenartowicz <r.lenartowicz123@gmail.com>
2017-10-07 23:46:33 +0200

    Fix templates path

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['D:/Code/projects_py/djangoTutorial/mysite/mysite/blog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
